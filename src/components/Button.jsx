a64135196a9e897c1604636db172b4574b89937f
SüheyL <h.suheylyazici@gmail.com>
2022-04-02 23:36:21 +0300

    project completed

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+const Button = ({ color, text, toggleShow }) => {
+
+  return (
+    <div>
+      <button
+        className="btn"
+        style={{ backgroundColor: color }}
+        onClick={toggleShow}
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import Button from "./Button";
+
+const Header = ({ title, showAddTask, toggleShow }) => {
+
+  return (
+    <div className="header">
+      <h1>{title}</h1>
+
+      <Button
+        color={showAddTask ? "red" : "purple"}
+        text={showAddTask ? "Close Add Task Bar" : "Show Add Task Bar"}
+        toggleShow={toggleShow}
+      />
+    </div>
+  );
+};
+
+export default Header;
